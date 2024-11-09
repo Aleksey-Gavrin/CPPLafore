fc8834eae1a5434d8d906a45a2cc11fd0dec74a0
Aleksey-Gavrin <aleksey.gavrin.mail@gmail.com>
2024-11-09 12:48:47 +0300

    resolve 1st task

---
+++ Chapter1/task1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    /*Задача 1:
+    Считая, что кубический фут равен 7,481 галлона, написать программу, запрашивающую у пользователя число галлонов и выводящую на экран эквива -
+    лентный объем в кубических футах.
+    */
+
+        cout << "Введите число галлонов:";
+        int gal_number;
+        cin >> gal_number;
+        double const MULTIPLIER = 7.481;
+        double cubic_foot_vol = gal_number * MULTIPLIER;
+        cout << "Объем в кубических футах: ";
+        cout << cubic_foot_vol << endl;
+
+    return 0;
+}
