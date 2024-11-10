a594ade45f80c6d5ae9e449c5391755f6fc95725
Aleksey-Gavrin <aleksey.gavrin.mail@gmail.com>
2024-11-10 11:41:40 +0300

    resolve task 4

---
+++ Chapter1/task4.cpp
@@ -0,0 +1,38 @@
+/*Задача 4:
+Напишите программу, выводящую на экран ваше любимое стихотворение. Для разбиения на строчки используйте подходящую управляющую
+последовательность. 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout    << "Day after day (your home life's a wreck)" << endl
+            << "The powers that be just breathe down your neck" << endl
+            << "You get no respect (you get no relief)" << endl
+            << "You gotta speak up and yell out your piece" << endl
+            << "So back off your rules, back off the jive" << endl
+            << "\'Cause I'm sick of not living to stay alive" << endl
+            << "Leave me alone, asking a lot" << endl
+            << "I don't wanna be controlled" << endl << endl
+            << "That's all I want" << endl << endl
+            << "How many times (is it gonna take)" << endl
+            << "\'Til someone around you hears what you say?" << endl
+            << "You've tried being cool (you feel like a lie)" << endl
+            << "You've played by their rules, now it's their turn to try" << endl
+            << "So back off your rules, back off your jive" << endl
+            << "\'Cause I'm sick of not living to stay alive" << endl
+            << "Leave me alone, not asking a lot" << endl
+            << "Just don't wanna be controlled" << endl << endl
+            << "That's all I want" << endl << endl
+            << "I said it before, I'll say it again" << endl
+            << "If you could just listen, then it might make sense" << endl
+            << "So back off your rules, back off your jive" << endl
+            << "\'Cause I'm sick of not living to stay alive" << endl
+            << "Leave me alone, not asking a lot" << endl
+            << "Just don't wanna be controlled" << endl << endl
+            << "That's all I want" << endl
+            << "                        " << "© " << "\"Offspring\"" << endl << endl;
+
+}
